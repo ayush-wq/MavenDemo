165cf2c03367884e80337e38459aaa2021720534
frescoplaylab <38917706+frescoplaylab@users.noreply.github.com>
2019-05-22 14:44:15 +0530

    Add files via upload

---
+++ src/test/java/com/fresco/play/AppTest.java
@@ -0,0 +1,20 @@
+ackage com.fresco.play;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
